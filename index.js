c90b68121472ada4c41401162d0eee14bee25066
SorrenJ <soalexjao@gmail.com>
2024-04-23 13:55:25 -0700

    refactor functions

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ function newTask(title, description) {
 
 }
 
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+
+
+
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 /*function completeTask(taskIndex) {
   taskComplete[taskIndex] = true;
@@ -44,6 +57,11 @@ function newTask(title, description) {
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
+
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
 
 // for now, let's just make sure we see our tasks
 console.log(tasks);
